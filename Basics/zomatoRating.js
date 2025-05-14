b61a6da727856037eb760f3b1db80f26ee0e9b54
diksha-jeena <diksha.jeena06@gmail.com>
2025-05-14 20:08:57 +0530

    feat:built basic doordash/zomato rating system

---
+++ Basics/zomatoRating.js
@@ -0,0 +1,10 @@
+let star = 3
+if(star == 1 || star == 2){
+    console.log("poor")
+}
+else if(star == 3){
+    console.log("average")
+}
+else if(star == 4 || star == 5){
+    console.log("good")
+}
